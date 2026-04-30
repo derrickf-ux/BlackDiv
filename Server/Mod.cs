22b3c602c4846050f785554165213d72ecb1304f
Matthew Christensen <christensenfamily05@gmail.com>
2026-04-29 22:35:48 -0500

    Fixed version and some config stuff

--- Server/Mod.cs
+++ Server/Mod.cs
@@ -18,7 +18,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "Black Division [REDACTED] Home";
     public override string Author { get; init; } = "TacticalToaster";
     public override List<string>? Contributors { get; init; } = new() { };
-    public override SemanticVersioning.Version Version { get; init; } = new(1, 0, 1);
+    public override SemanticVersioning.Version Version { get; init; } = new(1, 1, 0);
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = new()
